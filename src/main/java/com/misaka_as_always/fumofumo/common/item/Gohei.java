bcff9ac2bbff91e2ee996cc704a5f2051b4b8fd3
MisakaAsAlways <misaka.as.always@gmail.com>
2023-06-25 17:40:26 +0300

    🛠️ Changed gohei to sword type

--- src/main/java/com/misaka_as_always/fumofumo/common/item/Gohei.java
+++ src/main/java/com/misaka_as_always/fumofumo/common/item/Gohei.java
@@ -1,8 +1,10 @@
 package com.misaka_as_always.fumofumo.common.item;
 
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemTier;
+import net.minecraft.item.SwordItem;
 
-public class Gohei extends Item {
-    public Gohei(ItemGroup itemGroup) { super(new Properties().stacksTo(1).tab(itemGroup)); }
+public class Gohei extends SwordItem {
+    public Gohei(ItemGroup itemGroup) { super(ItemTier.DIAMOND, 3, 1.7F,
+            new Properties().stacksTo(1).durability(250).tab(itemGroup)); }
 }
